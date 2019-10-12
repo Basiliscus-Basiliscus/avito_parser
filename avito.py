223bc434aedceebdb43e836b340edabc131a4211
Ed <e.berbekov@gmail.com>
2019-10-12 21:11:39 +0300

    Now it can parse complicated items from avito

--- avito.py
+++ avito.py
@@ -3,8 +3,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-# https://www.avito.ru/sankt-peterburg?p=1&q=преподаватель+английского
+# a = 'https://www.avito.ru/sankt-peterburg?p=1&q=преподаватель+английского'
 # # 19 стр
 
 def get_html(url):
@@ -15,10 +14,17 @@ def get_html(url):
 def get_total_pages(html):
     soup = BeautifulSoup(html, 'lxml')
 
-    pages = soup.find('div',class_='pagination-pages').find_all('a',class_ = 'pagination-page')[-1].get('href')
+    pages = soup.find('div', class_ = 'pagination-pages').find_all('a',class_ = 'pagination-page')[-1].get('href')
     total_pages = pages.split('=')[1].split('&')[0]
 
     return int(total_pages)
+
+def get_page_data(html):
+    soup = BeautifulSoup(html, 'lxml')
+
+    ads = soup.find('div', class_ = 'catalog-list').find_all('div', class_ = 'item_table')
+    print(len(ads))
+
 
 def main():
     url = 'https://www.avito.ru/sankt-peterburg?p=1&q=преподаватель+английского'
@@ -27,10 +33,12 @@ def main():
     query_part = '&q=преподаватель+английского'
 
     total_pages = get_total_pages(get_html(url))
-    for i in range(1, total_pages+1):
+    #for i in range(1, total_pages+1):
+    for i in range(1, 3): #на первое время, для ускорения
         url_gen = base_url + page_part + str(i) + query_part
-        print(url_gen)
-    print(total_pages)
+        #print(url_gen)
+        html = get_html(url_gen)
+        get_page_data(html)
 
 
 if __name__ == "__main__":
